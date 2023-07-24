b313bbd621fe2acfe63a0ba6493497ffa6477452
Shay Johnson <shjohnson413@outlook.com>
2023-07-23 19:52:37 -0500

    Initial commit

---
+++ src/test/java/com/bar_app/login_service/LoginServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bar_app.login_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
